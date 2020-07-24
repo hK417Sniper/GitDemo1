59f9f4b12d48c93bd2667ff06dbe6845fdedbb74
Rui Hu <boreas955@gmail.com>
2020-07-24 11:44:33 +0800

    v0

---
+++ GitDemo1Java/src/GitDemo1.java
@@ -0,0 +1,12 @@
+public class GitDemo1 {
+
+    public static void main(String args[]){
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                System.out.print(((int)(1+2283*Math.random()))+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
